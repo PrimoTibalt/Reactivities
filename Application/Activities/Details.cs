a5f5f0f7313411e41b1944e1aab3ebb5ff44c480
TERMOPALE\anton <anton.shheglov.1@gmail.com>
2024-03-30 02:32:55 +0300

    End of section 10

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using Domain;
 using MediatR;
 using Persistence;
@@ -6,12 +7,12 @@ namespace Application.Activities
 {
     public class Details
     {
-        public class Query : IRequest<Activity>
+        public class Query : IRequest<Result<Activity>>
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query, Activity>
+        public class Handler : IRequestHandler<Query, Result<Activity>>
         {
             public DataContext DataContext { get; }
 
@@ -20,9 +21,10 @@ namespace Application.Activities
                 DataContext = dataContext;
             }
 
-            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await DataContext.Activities.FindAsync(request.Id);
+                var activity = await DataContext.Activities.FindAsync(request.Id);
+                return Result<Activity>.Success(activity);
             }
         }
     }
