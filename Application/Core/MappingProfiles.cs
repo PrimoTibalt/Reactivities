bcfaac1a5d5d71ae45de3e40ba4db24b839d26b8
TERMOPALE\anton <anton.shheglov.1@gmail.com>
2024-05-14 19:06:12 +0300

    End of section 14

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,3 +1,4 @@
+using Application.Activities;
 using AutoMapper;
 using Domain;
 
@@ -9,6 +10,13 @@ namespace Application.Core
         {
             CreateMap<Activity, Activity>()
                 .ForMember(a => a.Id, a => a.Ignore());
+            CreateMap<Activity, ActivityDto>()
+                .ForMember(d => d.HostUsername, o => o.MapFrom(a => 
+                    a.Attendees.FirstOrDefault(h => h.IsHost).AppUser.UserName));
+            CreateMap<ActivityAttendee, Profiles.Profile>()
+                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
+                .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
+                .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio));
         }
     }
 }
