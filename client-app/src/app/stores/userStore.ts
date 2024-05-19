c2f649fb33a2e4fc6f8ec77ddbf320e56a87df7c
TERMOPALE\anton <anton.shheglov.1@gmail.com>
2024-05-19 21:19:05 +0300

    End of section 17

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -45,5 +45,9 @@ export default class UserStore {
             console.log(error);
         }
     }
+
+    setImage = (image: string) => {
+        if (this.user) this.user.image = image;
+    }
 }
 
