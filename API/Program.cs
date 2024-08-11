18d58a4012f8388c14b38cd51647a3338a4ac2d4
TERMOPALE\anton <anton.shheglov.1@gmail.com>
2024-08-11 20:27:51 +0300

    Improve Security Policies for https

--- API/Program.cs
+++ API/Program.cs
@@ -28,14 +28,14 @@ app.UseXContentTypeOptions();
 app.UseReferrerPolicy(opt => opt.NoReferrer());
 app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
 app.UseXfo(opt => opt.Deny());
-app.UseCspReportOnly(opt => opt
+app.UseCsp(opt => opt
     .BlockAllMixedContent()
-    .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com", "https://accounts.google.com"))
+    .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com", "sha256-lmto2U1o7YINyHPg9TOCjIt+o5pSFNU/T2oLxDPF+uw=", "https://accounts.google.com/gsi/style"))
     .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
     .FormActions(s => s.Self())
     .FrameAncestors(s => s.Self())
-    .ImageSources(s => s.Self().CustomSources("blob:", "https://res.cloudinary.com"))
-    .ScriptSources(s => s.Self())
+    .ImageSources(s => s.Self().CustomSources("blob:", "https://res.cloudinary.com", "data:", "https://lh3.googleusercontent.com"))
+    .ScriptSources(s => s.Self().CustomSources("https://accounts.google.com"))
 );
 if (app.Environment.IsDevelopment())
 {
